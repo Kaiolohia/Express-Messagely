9fbb7b8a5fb8582954a9629fcc1281bb42b246b6
TruxSux <kaithomasdean@outlook.com>
2021-11-10 14:01:37 -1000

    Completed

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 /** User class for message.ly */
 
-
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const { BCRYPT_WORK_FACTOR } = require("../config")
 
 /** User of the site. */
 
@@ -10,20 +12,53 @@ class User {
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+  static async register({username, password, first_name, last_name, phone}) {
+      password = await bcrypt.hash(password, BCRYPT_WORK_FACTOR)
+      const results = await db.query(`INSERT INTO users (
+        username, 
+        password, 
+        first_name, 
+        last_name, 
+        phone, 
+        join_at, 
+        last_login_at
+        ) 
+        VALUES ($1, $2, $3, $4, $5, NOW(), NOW())
+        RETURNING username, 
+        password, 
+        first_name, 
+        last_name,
+        phone`, [
+          username,
+          password,
+          first_name,
+          last_name,
+          phone,
+        ]);
+      return results.rows[0]
+  }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+  static async authenticate(username, password) {
+    const result = await db.query("SELECT password FROM users WHERE username=$1", [username]);
+    const found_password = result.rows[0].password
+    return await bcrypt.compare(password, found_password);
+  }
 
   /** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+  static async updateLoginTimestamp(username) {
+    await db.query("UPDATE users SET last_login_at = NOW()")
+  }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
 
-  static async all() { }
+  static async all() {
+    const result = await db.query("SELECT username, first_name, last_name, phone FROM users")
+    return result.rows;
+  }
 
   /** Get: get user by username
    *
@@ -34,7 +69,10 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+  static async get(username) {
+    const result = await db.query("SELECT username, first_name, last_name, phone, join_at, last_login_at FROM users WHERE username=$1", [username])
+    return result.rows[0]
+  }
 
   /** Return messages from this user.
    *
@@ -44,7 +82,21 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) { }
+  static async messagesFrom(username) {
+    const result = await db.query("SELECT m.id, m.to_username, m.body, m.sent_at, m.read_at, u.first_name, u.last_name, u.phone FROM messages AS m LEFT JOIN users AS u ON m.to_username = u.username WHERE from_username=$1", [username])
+    return result.rows.map(r => ({
+      id : r.id,
+      to_user: {
+        username : r.to_username,
+        first_name : r.first_name,
+        last_name : r.last_name,
+        phone : r.phone
+      },
+      body : r.body,
+      sent_at : r.sent_at,
+      read_at : r.read_at
+    }))
+  }
 
   /** Return messages to this user.
    *
@@ -54,7 +106,21 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) { }
+  static async messagesTo(username) {
+    const result = await db.query("SELECT m.id, m.from_username, m.body, m.sent_at, m.read_at, u.first_name, u.last_name, u.phone FROM messages AS m LEFT JOIN users AS u ON m.from_username = u.username WHERE to_username=$1", [username])
+    return result.rows.map(r => ({
+      id : r.id,
+      from_user: {
+        username : r.from_username,
+        first_name : r.first_name,
+        last_name : r.last_name,
+        phone : r.phone
+      },
+      body : r.body,
+      sent_at : r.sent_at,
+      read_at : r.read_at
+    }))
+  }
 }
 
 
